1d7f322845e684689bce10e5c85985e37dcda4a1
Elfgirl21 <122906174+Elfgirl21@users.noreply.github.com>
2023-05-10 11:15:47 -0500

    Add files via upload

---
+++ alien_invasion/ship.py
@@ -0,0 +1,46 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Ship(Sprite):
+    """A class to manage the ship."""
+
+    def __init__(self, ai_game): #ai_game current instance of the AlienInvasion class
+        """Initialize the ship and set its starting position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect() #place ship in correct location
+
+        #Load the ship image and get its rect.
+        self.image = pygame.image.load('alien_invasion\images\ship.bmp')
+        self.rect = self.image.get_rect() #rectangle - rect
+
+        #start each new ship at bottom centre of screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        #store a decimal value for the ship's horiztonal position
+        self.x = float(self.rect.x)
+
+        #Movement flags
+        self.moving_right = False
+        self.moving_left = False
+    
+    def update(self): #not a helper method, called through instance of Ship
+        """Update the ship's position based on the movement flag."""
+        #Update the ship's x value, not the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right: #self.rect.right - ships right edge
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+       #Update rect object from self.x
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
